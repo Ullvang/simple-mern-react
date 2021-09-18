2ca7a04d0e412e21958e9768bac982bdcd0e359a
Jon Erik <jon.erik.ullvang@gmail.com>
2021-09-18 13:47:10 +0200

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    axios
+      .get(process.env.REACT_APP_SERVER + "/tests")
+      .then((res) => console.log(res));
+    axios
+      .get(process.env.REACT_APP_SERVER + "/users")
+      .then((res) => console.log(res));
+    axios
+      .get(process.env.REACT_APP_SERVER + "/")
+      .then((res) => console.log(res));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>MERN</h1>
     </div>
   );
 }
